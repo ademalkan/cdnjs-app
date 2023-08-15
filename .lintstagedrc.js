b58a6444a5d3cdc268ecd91a4bdb9d05a430f89f
ademalkan <ademalkan@distedavim.com>
2023-08-15 22:54:35 +0300

    setup starter kit

---
+++ .lintstagedrc.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+const cypressRunCommand = "yarn run cypress run";
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};
